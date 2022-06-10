186b0d5406cf5dfd318ec9a23d5aca116fed4afe
Lolloc99 <lolloc99@yahoo.it>
2022-06-10 15:39:43 +0200

    Milestone 1 completata

--- Milestone-1/index.php
+++ Milestone-1/index.php
@@ -1,3 +1,9 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,6 +15,30 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <?php ?>
+    <!-- 
+        Prima Milestone:
+        Stampiamo i dischi solo con l’utilizzo di PHP, che stampa direttamente i dischi in pagina: al caricamento della pagina ci saranno tutti i dischi.
+     -->
+    <?php 
+    require __DIR__ . "/partials/database.php";
+    ?>
+
+    <header>
+        <img src="img/logo-small.svg" alt="Logo Spotify" />
+    </header>
+    <main>
+        <div class="container">
+            <?php
+            foreach ($database as $element) {
+            ?>
+            <div class="card">
+                <img src="<?php echo $element["poster"] ?>" alt="<?php echo $element["title"] ?>" />
+                <h3><?php echo $element["title"] ?></h3>
+                <p><?php echo $element["author"] ?></p>
+                <small><?php echo $element["year"] ?></small>
+            </div>
+            <?php } ?>
+        </div>
+    </main>
 </body>
 </html>
